daf8fe3ce88aad2de44b8abaf93371e512e0bd06
Himanshu Soni <64693663+himanshu2399@users.noreply.github.com>
2025-03-27 09:42:25 +0530

    Update app.py

--- service-a/app.py
+++ service-a/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Service A, welcome to my world!"
+    return "Hello from Service A, welcome to my world!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
